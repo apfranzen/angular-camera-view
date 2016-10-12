493dc80760de759eb73db6f432b8bda5a8ea93fa
Adam Franzen <adamfranzen@gmail.com>
2016-10-12 11:24:33 -0600

    part 1 complete

--- js/app.js
+++ js/app.js
@@ -1,5 +1,16 @@
 (function() {
   'use strict';
+  var app = angular.module('cameraApp', []);
+
+  app.filter('orderStars', () => {
+    return (number) => {
+      let stars = '';
+      for (var i = 0; i < number; i++) {
+        stars += '*';
+      }
+      return stars;
+    }
+  });
 
   $('select').material_select();
 
